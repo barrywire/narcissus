0d3dc74628bc08c30d540771fd9d3eeb7e150614
barrywire <bmwemawire@gmail.com>
2022-08-26 23:32:07 +0300

    chore: boilerplte clean up

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,23 @@
+// React imports
 import React from 'react';
+
+// React DOM imports
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// Styling imports
+import './styles/index.css';
+
+// Component imports
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Package imports
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
