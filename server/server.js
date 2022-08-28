6fa1cad2159e4505d40a601818f09ab3d6c220c1
barrywire <bmwemawire@gmail.com>
2022-08-28 19:35:21 +0300

    chore: get firebase data in server.js

--- server/server.js
+++ server/server.js
@@ -5,10 +5,33 @@ const app = express()
 // Set up middleware
 app.use(express.json())
 
-
 // Import the other packages
 const cron = require('node-cron')
 const nodemailer = require('nodemailer')
+const admin = require('firebase-admin')
+const credentials = require('./narcissus-rjw-adminsdk.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+    databaseURL: 'https://narcissus-rjw-default-rtdb.firebaseio.com'
+})
+
+app.post('/signup', async (req, res) =>
+{
+    const userResponse = await admin.auth().createUser({
+        email: req.body.email,
+        password: req.body.password,
+        emailVerified: false,
+        disabled: false,
+    })
+    res.json(userResponse)
+})
+
+app.get('/users', async (req, res) =>
+{
+    const users = await admin.auth().listUsers()
+    res.json(users)
+})
 
 // Set up the port and listener for the requests
 const PORT = process.env.PORT || 8080
@@ -40,15 +63,15 @@ var mailOptions = {
 };
 
 
-// create a cron scheduler to run every minute
-cron.schedule('* */20 * * * *', () =>
-{
-    // transporter.sendMail(mailOptions, function (err, info)
-    // {
-    //     if (err)
-    //         console.log('Error ', err);
-    //     else
-    //         console.log('Info ', info.response);
-    // })
-    console.log('Running every minute');
-})
+// // create a cron scheduler to run every minute
+// cron.schedule('* */20 * * * *', () =>
+// {
+//     // transporter.sendMail(mailOptions, function (err, info)
+//     // {
+//     //     if (err)
+//     //         console.log('Error ', err);
+//     //     else
+//     //         console.log('Info ', info.response);
+//     // })
+//     console.log('Running every minute');
+// })
